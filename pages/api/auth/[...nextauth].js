40dd21658b95262081b9f921b14ad9257f6f6d08
moonishkale <manishkhadgi123@gmail.com>
2023-06-03 15:36:06 +1000

    google auth feature added

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,15 @@
+import clientPromise from "@/lib/mongodb";
+import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+});
