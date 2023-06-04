af832102e81ff4b75397b5fef3ed6fd3b81bd888
moonishkale <manishkhadgi123@gmail.com>
2023-06-04 20:17:17 +1000

    product and order page added

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,13 @@
-import Nav from "@/components/nav";
-import { useSession, signIn, signOut } from "next-auth/react";
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
-  if (!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button
-            onClick={() => signIn("google")}
-            className="bg-white p-2  px-4 rounded-lg"
-          >
-            Login with Google
-          </button>
-        </div>
-      </div>
-    );
-  }
+  console.log({ session });
 
   return (
-    <div className="bg-blue-900 min-h-screen">
-      <Nav />
-      <div> Logged in {session.user.email} </div>
-    </div>
+    <Layout>
+      <div className="text-blue-900"> Hello, {session?.user?.name}</div>
+    </Layout>
   );
 }
