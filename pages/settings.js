70f07685dd5be622b93b6285cea4e001eed4b41f
moonishkale <manishkhadgi123@gmail.com>
2023-06-23 21:15:07 +1000

    changed setting

--- pages/settings.js
+++ pages/settings.js
@@ -3,7 +3,9 @@ import Layout from '@/components/Layout';
 export default function settings() {
   return (
     <Layout>
-      <div className="flex items-center"> My Profile</div>
+      <div className="flex items-center"> My Account</div>
+      <div className="flex items-center"> Edit Account</div>
+      <div className="flex items-center"> My Orders</div>
     </Layout>
   );
 }
